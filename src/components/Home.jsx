f922d3054520ce99bc207ecca72362e8424a7d54
Abdullah2000Habib <abd3habib@gmail.com>
2022-12-26 16:01:21 +0200

    Hero Section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Hero from './Hero/Hero';
 
 export default function Home() {
   return (
     <div>
         
         <Header/>
+        <Hero/>
         
     </div>
   )
