7a4ee2a83da65e52e53e72ff319f596b2fadb9ee
Abdullah2000Habib <abd3habib@gmail.com>
2022-12-26 12:45:08 +0200

    Starter Pack Overview

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import "./main.css";
+
+
+
+
+function App() {
+  return (
+
+    <>
+
+    <span>App</span>
+
+    </>
+          
+  );
+  
+  }
+
+export default App;
